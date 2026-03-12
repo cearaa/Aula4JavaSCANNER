4e8d55c4b120a93331ffe1818704fb72b81fe733
Rikk <tarikmoussa90@gmail.com>
2026-03-11 22:43:08 -0300

    Atividade aula 4 de Java, sobre Scanner, janelas pelo JOption... dia 11/03/2026

--- src/br/com/fiap/Exercício1Scanner.java
+++ src/br/com/fiap/Exercício1Scanner.java
@@ -17,7 +17,7 @@ public class Exercício1Scanner {
             media = p1 + p2 + p3 + p4 /4;
             System.out.println("Prova 1: " + p1 + "\nProva 2: " + p2 + "\nProva 3: " + p3 + "\nProva 4: " + p4 + "\nMédia: " + media);
         } catch (Exception e) {
-            System.out.println("Formato de número incorreto");
+            System.out.println("Formato de número incorreto.");
         }
     }
 }
